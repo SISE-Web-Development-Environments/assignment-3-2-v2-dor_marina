a68ef284a946c9318686b0cec5314aae733db559
dorelkab <48754168+dorelkab@users.noreply.github.com>
2020-07-12 11:41:23 +0300

    some changes

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="container">
     <h1 class="title">Search Page</h1>
+    <b-form class="flex" @submit.prevent="onSearch">
+      <b-form-group
+        id="input-group-search"
+        label-cols-sm="4"
+        label="Search recipe:"
+        label-for="Search"
+      >
+        <b-form-input
+          id="keyword"
+          v-model="$v.form.Keyword.$model"
+          type="text"
+          style="width:250px;"
+          :state="validateState('Keyword')"
+        ></b-form-input>
+        <b-form-invalid-feedback>
+          Keyword is required
+        </b-form-invalid-feedback>
+      </b-form-group>
+      <b-button
+        type="search"
+        variant="primary"
+        style="width:100px;height:40px"
+        class="ml-5"
+        >Register</b-button
+      >
+    </b-form>
   </div>
 </template>
+
+
+<script>
+  import { required } from "vuelidate/lib/validators";
+  export default {
+    name: "Login",
+    data() {
+    return {
+    form: {
+      Keyword:"",
+      submitError: undefined
+    }
+    };
+  },
+  validations: {
+    form: {
+      Keyword:{
+       required
+      }
+    }
+  },
+  methods: {
+    validateState(param) {
+      const { $dirty, $error } = this.$v.form[param];
+      return $dirty ? !$error : null;
+    }
+  }
+  }
+</script>
+<style lang="scss" scoped>
+.flex {
+    display: flex;
+}
+</style>
